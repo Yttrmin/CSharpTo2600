0122092e98056228ff50fa86811a3a6d56c749fb
Yttrmin <Yttrmin@users.noreply.github.com>
2015-02-04 14:29:35 -0500

    Remove pointless line from functionality test

--- FunctionalitySamples/InitializerChangeBkColor.cs
+++ FunctionalitySamples/InitializerChangeBkColor.cs
@@ -7,7 +7,6 @@ namespace CSharpTo2600.FunctionalitySamples
     static class SingleChangeBkColor
     {
         [SpecialMethod(MethodType.Initialize)]
-        [System.Obsolete(CSharpTo2600.Framework.Assembly.Symbols.AUDC0, true)]
         static void Initialize()
         {
             BackgroundColor = 0x5E;
