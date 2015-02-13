8f60608bf736bb0184e54a4297b2eb3d4d099844
Yttrmin <Yttrmin@users.noreply.github.com>
2015-02-13 00:05:49 -0500

    Add unit test for Fragments.PushLiteral

--- UnitTests6502/FragmentTests.cs
+++ UnitTests6502/FragmentTests.cs
@@ -1,6 +1,9 @@
-﻿using CSharpTo2600.Compiler;
+﻿using System.Runtime.InteropServices;
+using System;
+using System.Linq;
 using System.Collections.Generic;
 using Xunit;
+using CSharpTo2600.Compiler;
 using CSharpTo2600.Framework.Assembly;
 using static CSharpTo2600.Framework.Assembly.AssemblyFactory;
 
@@ -30,31 +33,90 @@ namespace CSharpTo2600.UnitTests
         [Fact]
         public void SystemIsCleared()
         {
-            var ClearCode = new List<AssemblyLine>();
-            ClearCode.Add(AssemblyFactory.Processor());
-            ClearCode.Add(Include("vcs.h"));
-            ClearCode.Add(Org(0xF000));
-            ClearCode.Add(ProgramEnd);
-            ClearCode.AddRange(Fragments.ClearSystem());
-            ClearCode.Add(JMP(ProgramEnd));
-            CPU.LoadProgram(0xF000, ROMBuilder.BuildRawROM(ClearCode), 0xF000);
-            while(CPU.ProgramCounter != ProgramEnd.Value.Value)
-            {
-                CPU.NextStep();
-            }
+            RunProgramFromFragmentWithoutClearingSystem(Fragments.ClearSystem());
 
             // Make sure memory was reset to 0.
             for(var i = 0; i < 128; i++)
             {
                 Assert.Equal(0, CPU.Memory.ReadValue(i));
             }
-
             // Make sure interrupts are disabled.
             Assert.True(CPU.DisableInterruptFlag);
             // Make sure decimal mode is cleared.
             Assert.False(CPU.DecimalFlag);
             // Make sure stack pointer = 0xFF.
             Assert.Equal(0xFF, CPU.StackPointer);
+        }
+
+        [Theory]
+        [InlineData((byte)0xEF)]
+        [InlineData((int)0x7FABCDEF)]
+        [InlineData((ulong)0xDEADBEEFBAADF00D)]
+        public void PushLiteral(object Input)
+        {
+            RunProgramFromFragment(Fragments.PushLiteral(Input));
+            var Size = Marshal.SizeOf(Input.GetType());
+            var Ptr = Marshal.AllocHGlobal(Size);
+            var ByteArray = new byte[Size];
+            Marshal.StructureToPtr(Input, Ptr, false);
+            Marshal.Copy(Ptr, ByteArray, 0, ByteArray.Length);
+            Marshal.FreeHGlobal(Ptr);
+            // ByteArray contains the bytes of the literal in big-endian.
+            Array.Reverse(ByteArray);
+
+            // Where the top of the stack should be based on size of literal.
+            var ExpectedStackPointer = 0xFF - Size;
+            // On a 6502 the stack is hardwired to 0x100-0x1FF, which is a mirror of
+            // 0x000-0x0FF on the 2600.
+            // Remember SP points to where the next pushed byte would go. Not where
+            // the last byte that was pushed is located.
+            var ExpectedLiteralStart = ExpectedStackPointer + 1 + 0x100;
+            
+            // Ensure SP is correct.
+            Assert.Equal(ExpectedStackPointer, CPU.StackPointer);
+            // Contains the bytes of the literal from the 6502 in big-endian.
+            var BigEndianBytes = new byte[Size];
+            Array.ConstrainedCopy(CPU.Memory.DumpMemory(), ExpectedLiteralStart, BigEndianBytes, 0, Size);
+            // Ensure bytes are pushed onto stack in big-endian.
+            Assert.True(ByteArray.SequenceEqual(BigEndianBytes));
+        }
+
+        private void RunProgramFromFragment(IEnumerable<AssemblyLine> FragmentLines)
+        {
+            var Lines = new List<AssemblyLine>();
+            Lines.Add(AssemblyFactory.Processor());
+            Lines.Add(Include("vcs.h"));
+            Lines.Add(Org(0xF000));
+            Lines.Add(ProgramEnd);
+            // Need to use this to ensure SP = 0xFF and all the other stuff.
+            // Confirmed to do its job by another unit test.
+            Lines.AddRange(Fragments.ClearSystem());
+            Lines.AddRange(FragmentLines);
+            Lines.Add(JMP(ProgramEnd));
+
+            CPU.LoadProgram(0xF000, ROMBuilder.BuildRawROM(Lines), 0xF000);
+            while (CPU.ProgramCounter != ProgramEnd.Value.Value)
+            {
+                CPU.NextStep();
+            }
+        }
+
+        // Use this when testing Fragments.ClearSystem() only.
+        private void RunProgramFromFragmentWithoutClearingSystem(IEnumerable<AssemblyLine> FragmentLines)
+        {
+            var Lines = new List<AssemblyLine>();
+            Lines.Add(AssemblyFactory.Processor());
+            Lines.Add(Include("vcs.h"));
+            Lines.Add(Org(0xF000));
+            Lines.Add(ProgramEnd);
+            Lines.AddRange(FragmentLines);
+            Lines.Add(JMP(ProgramEnd));
+
+            CPU.LoadProgram(0xF000, ROMBuilder.BuildRawROM(Lines), 0xF000);
+            while (CPU.ProgramCounter != ProgramEnd.Value.Value)
+            {
+                CPU.NextStep();
+            }
         }
     }
 }
