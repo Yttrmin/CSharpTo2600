08f53e326f9cd210c39475897b03c8436d70ccee
Yttrmin <Yttrmin@users.noreply.github.com>
2015-02-14 01:18:47 -0500

    Move common code from FragmentTests to new AssemblyTests class
    - Git thinks this is some weird rename/add combo. Hope that doesn't actually
    mess anything up.

---
+++ UnitTests6502/InstructionTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTo2600.UnitTests
+{
+    class InstructionTests : AssemblyTests
+    {
+    }
+}
