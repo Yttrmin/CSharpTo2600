36e71860f946f417f9a46cdfc7d788a07e833e28
Yttrmin <Yttrmin@users.noreply.github.com>
2018-03-18 20:52:48 -0400

    Add sample to illustrate lack of ordering of type compilation, for a future branch.

---
+++ Samples/TypeCompilationOrderTesting.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VCSFramework;
+
+namespace Samples
+{
+	public static class Foo
+	{
+		[AlwaysInline]
+		public static void Run()
+		{
+
+		}
+	}
+
+	// TODO - This is not supported yet.
+	public static class TypeCompilationOrderTesting
+    {
+		private static byte TestByte;
+
+		// AlwaysInline exacerbates the issue since since it requires the method to already be compiled by the
+		// time it finds any call sites for it.
+		public static void Main()
+		{
+			Foo.Run();
+			TestByte++;
+			Bar.Run();
+		}
+	}
+
+	public static class Bar
+	{
+		[AlwaysInline]
+		public static void Run()
+		{
+
+		}
+	}
+}
