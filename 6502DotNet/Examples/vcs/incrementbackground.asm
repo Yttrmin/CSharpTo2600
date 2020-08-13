4dbf2921fddc410b6f5370d9ab9cc550325ee83e
Yttrmin <Yttrmin@users.noreply.github.com>
2020-08-13 02:25:14 -0400

    Add a couple extremely stripped-down reference ASM files and C# file, intent is to target a program like these at first to flesh out design issues, versus having to build out all the required features of a proper VCS program at first.

---
+++ 6502DotNet/Examples/vcs/incrementbackground.asm
@@ -0,0 +1,24 @@
+﻿.cpu "6502"
+.format "flat"
+* = $F000
+
+COLUBK = $09
+
+// Repeatedly increments COLUBK without making any effort to handle scanlines, perform VSYNC,
+// etc. Result is a rapidly scrolling screen that changes color multiple times mid-scanline.
+// Only tested on emulator, not on TVs.
+// This is not a proper VCS program. It's just meant to see how stripped down of a program
+// we can make that still displays something on the screen.
+Start 
+	SEI
+	CLD
+	LDX #0
+MainLoop
+	STX COLUBK
+	INX
+	JMP MainLoop
+
+// Special memory locations. Tells the 6502 where to go.
+* = $FFFC
+.word Start
+.word Start
---
+++ Samples/SimpleSolidBackgroundColor.cs
@@ -0,0 +1,15 @@
+﻿using static VCSFramework.Registers;
+
+namespace Samples
+{
+    // Not a proper VCS program.
+    // Displays a constant white background (on emulator, not tested on TVs).
+    static class SimpleSolidBackgroundColor
+    {
+        public static void Main()
+        {
+            ColuBk = 0x0E;
+            while (true) ;
+        }
+    }
+}
